bebe0443d9044a6f8a1e2425a76bacaddb52f733
denisskin <denis@begom.com>
2016-05-09 05:06:13 +0300

    load_whois_info

--- whois.go
+++ whois.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"io/ioutil"
 	"net"
+	"regexp"
 	"strings"
 	"time"
 )
@@ -41,4 +42,67 @@ func LoadRawDomainInfo(domain string) ([]byte, error) {
 	}
 
 	return ioutil.ReadAll(conn)
+}
+
+func LoadDomainInfo(domain string) (*WhoisInfo, error) {
+	if data, err := LoadRawDomainInfo(domain); err != nil {
+		return nil, err
+	} else {
+		return ParseWhoisInfo(domain, data), nil
+	}
+}
+
+type WhoisInfo struct {
+	Domain  string
+	RawData []byte
+	Params  map[string][]string
+}
+
+var reParam = regexp.MustCompile(`[\r\n]\s*([a-zA-Z0-9\- /\\_]+):\s*(.*)`)
+
+func normalizeParamName(name string) string {
+	name = strings.TrimSpace(name)
+	name = strings.ToLower(name)
+	name = strings.Replace(name, "-", " ", -1)
+	return name
+}
+
+func ParseWhoisInfo(domain string, data []byte) *WhoisInfo {
+	mm := reParam.FindAllStringSubmatch(string(data), -1)
+	if len(mm) == 0 {
+		return nil
+	}
+	inf := &WhoisInfo{
+		Domain:  domain,
+		RawData: data,
+		Params:  map[string][]string{},
+	}
+	for _, m := range mm {
+		name := normalizeParamName(m[1])
+		inf.Params[name] = append(inf.Params[name], strings.TrimSpace(m[2]))
+	}
+	return inf
+}
+
+func (i *WhoisInfo) GetParam(name string) string {
+	if ss := i.Params[normalizeParamName(name)]; len(ss) > 0 {
+		return ss[0]
+	}
+	return ""
+}
+
+func (i *WhoisInfo) NameServers() (v []string) {
+	v = append(v, i.Params["n server"]...)
+	v = append(v, i.Params["name server"]...)
+	return
+}
+
+func (i *WhoisInfo) WhoisServer() (v string) {
+	if v = i.GetParam("whois"); v != "" {
+		return
+	}
+	if v = i.GetParam("whois server"); v != "" {
+		return
+	}
+	return
 }
