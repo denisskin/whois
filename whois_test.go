e3d9295d0cf312159d419e4647fed05212577d28
denisskin <denis@begom.com>
2017-02-26 23:20:04 +0300

    add unit-test;

--- whois_test.go
+++ whois_test.go
@@ -12,3 +12,12 @@ func TestLoadRawDomainInfo(t *testing.T) {
 	assert.Contains(t, string(inf), "Domain Name: GOLANG.ORG")
 	assert.Contains(t, string(inf), "Creation Date: 2009-08-21T20:27:16Z")
 }
+
+func TestLoadDomainInfo(t *testing.T) {
+	inf, err := LoadDomainInfo("golang.org")
+
+	assert.NoError(t, err)
+	assert.True(t, len(inf.Params) > 0)
+	assert.Equal(t, "2009-08-21T20:27:16Z", inf.GetParam("Creation Date"))
+	assert.True(t, len(inf.NameServers()) > 0)
+}
